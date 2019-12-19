1deedaa0417e39034152b4efdbe76941e021bae5
elvis-chuks <celvischuks@gmail.com>
2019-12-19 18:14:52 +0100

    fixed Procfile

--- main.go
+++ main.go
@@ -16,44 +16,47 @@ func ussd_callback(w http.ResponseWriter, r *http.Request){
 	// session_id := r.URL.Query()["sessionId"][0]
 	// service_code := r.URL.Query()["serviceCode"][0]
 	// phone_number := r.URL.Query()["phoneNumber"][0]
-	texts,ok := r.URL.Query()["text"]
-	if !ok || len(texts[0]) < 1 {
-		w.Write([]byte("invalid request"))
-	}
-	else{
-		text := texts[0]
-	}
+	
 	
 
-	// _ = fmt.Sprintf("%s,%s,%s,%s",session_id,service_code,phone_number,text)
 
 
+	// if len(text) == 0{
 
-	if len(text) == 0{
+	// 	w.Write([]byte("CON Gplang Ussd \n1. Send Money\n2. Buy Airtime"))
 
+	// }
+	texts,ok := r.URL.Query()["text"]
+	if !ok || len(texts[0]) < 1 {
 		w.Write([]byte("CON Gplang Ussd \n1. Send Money\n2. Buy Airtime"))
-
-	}else{
+		return
+	}
+	text := texts[0]
 
 		switch text{
 
 		case "1":
 			w.Write([]byte("CON Select Bank \n1. FBN\n2. UBA"))
+			return
 
 		case "2":
 			w.Write([]byte("CON Select ISP \n1. MTN \n2. Airtel \n3. Glo"))
+			return
 
 		case "1*1":
 			w.Write([]byte("END Success"))
+			return
 
 		case "1*2":
 			w.Write([]byte("END Airtime purchase succesful"))
+			return
 
 		default:
 			w.Write([]byte("END Invalid input"))
+			return
 
 		}
-	}
+	
 	
 
 }
